d9d58388cd924d6d06e2f2319ab15e701caa5678
Gary Lu <madcated.lu@gmail.com>
2021-06-12 00:01:03 -0700

    Add transaction table

--- src/app/account-detail/account-detail.component.ts
+++ src/app/account-detail/account-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, Observable } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { Account, AccountService } from 'src/app/shared/account.service';
 import { DEFAULT_ICON, getAccountTypes$ } from '../shared/account-type';
@@ -16,6 +17,10 @@ export class AccountDetailComponent implements OnInit {
   public account$: Observable<Account>;
   public accountIcon$: Observable<string>;
 
+  displayedColumns: string[] = ['transactionDate', 'postingDate', 'type', 'amount', 'description', 'balance', 'action'];
+
+  transactionDataSource = new TransactionDataSource();
+
   constructor(private route: ActivatedRoute, private accountService: AccountService) {
     this.accountId$ = this.route.params.pipe(map((params) => params.id));
 
@@ -30,9 +35,45 @@ export class AccountDetailComponent implements OnInit {
     );
 
     this.accountIcon$ = combineLatest([this.account$, getAccountTypes$()]).pipe(
-      map(([account, types]) => types.find(type => type.value === account.type)?.icon || DEFAULT_ICON)
+      map(([account, types]) => types.find((type) => type.value === account.type)?.icon || DEFAULT_ICON)
     );
   }
 
   ngOnInit(): void {}
+}
+
+export interface Transaction {
+  transactionDate: Date;
+  postingDate: Date;
+  type: string;
+  amount: number;
+  description: string;
+  balance: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionDataSource extends DataSource<Transaction> {
+  constructor() {
+    super();
+  }
+
+  connect(): Observable<Transaction[]> {
+    var transactions: Transaction[] = [];
+    for (var i = 0; i < 50; i++) {
+      transactions.push({
+        transactionDate: new Date(),
+        postingDate: new Date(),
+        type: 'TEST',
+        amount: 100,
+        description: 'TEST',
+        balance: 100,
+      });
+    }
+
+    return of(transactions);
+  }
+
+  disconnect() {}
 }
