e90490a5f9ed3b6e7e0a247f6fdb433c016ce79c
Gary Lu <madcated.lu@gmail.com>
2021-07-03 00:08:14 -0700

    Match file fields with the system fields

--- src/app/account-detail/import-transactions-dialog/import-transactions-dialog.component.ts
+++ src/app/account-detail/import-transactions-dialog/import-transactions-dialog.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/storage';
-import { v4 as uuidv4 } from 'uuid';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+import { v4 as uuidv4 } from 'uuid';
+import { FieldOption } from './file-field-mapper/file-field-mapper.component';
 
 @Component({
   selector: 'app-import-transactions-dialog',
@@ -15,19 +18,52 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
     },
   ],
 })
-export class ImportTransactionsDialogComponent implements OnInit {
+export class ImportTransactionsDialogComponent implements OnInit, OnDestroy {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private fireStorage: AngularFireStorage) {
+  private transactionsFile$: Observable<File>;
+  private fileHeader$$: BehaviorSubject<FieldOption[]> = new BehaviorSubject<FieldOption[]>([]);
+
+  private ngUnsubscribe = new Subject();
+
+  constructor(private fireStorage: AngularFireStorage) {
     this.firstFormGroup = new FormGroup({
       transactionFile: new FormControl(null, [Validators.required]),
     });
 
-    this.secondFormGroup = this.formBuilder.group({ fileName: '' });
+    this.secondFormGroup = new FormGroup({
+      transactionDate: new FormControl('', Validators.required),
+      postedDate: new FormControl(-1, Validators.required),
+      type: new FormControl(-1, Validators.required),
+      amount: new FormControl('', Validators.required),
+      description: new FormControl(-1, Validators.required),
+    });
+
+    this.transactionsFile$ = this.firstFormGroup.get('transactionFile')!.valueChanges.pipe(
+      tap((file) => {
+        const fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          if (typeof fileReader.result === 'string') {
+            this.fileHeader$$.next(this.parseFileHeader(fileReader.result));
+          }
+        };
+        // TODO: handle errors
+
+        fileReader.readAsText(file);
+      }),
+      takeUntil(this.ngUnsubscribe)
+    );
+
+    this.transactionsFile$.subscribe();
   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
   importTransactions(): void {}
 
@@ -40,5 +76,20 @@ export class ImportTransactionsDialogComponent implements OnInit {
       .then((ref) => {
         console.log('uploaded', ref);
       });
+  }
+
+  private parseFileHeader(fileContent: string): FieldOption[] {
+    const header = fileContent.split('\n')[0];
+    return header
+      ? header.split(',').map((field, idx) => ({ name: field.trim().replace(/^\"+|\"+$/g, ''), value: idx }))
+      : [];
+  }
+
+  get fileHeader$(): Observable<FieldOption[]> {
+    return this.fileHeader$$.asObservable();
+  }
+
+  test(): void {
+    console.log(this.secondFormGroup.value);
   }
 }
